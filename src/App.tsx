d619e1d6ca686638f52f923d88eae315dbf3be48
Romaric Zounlome <rzounlom@gmail.com>
2024-09-03 19:41:10 -0400

    complete demo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Navbar from "./components/nav/Navbar";
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 Flexbox Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
